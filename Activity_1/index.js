df48fb139300316ad4855e2e44ff539f9fcff35e
devaryal96 <60721669+devaryal96@users.noreply.github.com>
2020-04-07 21:57:06 -0500

    Activity1Push

---
+++ Activity_1/index.js
@@ -0,0 +1,55 @@
+//The product of 9 and 9
+
+let x = 9;
+let y = 9;
+let output = (x*y);
+console.log("The product of 9 and 9 is : "+ output);
+
+
+
+//The quotient of 56 and 12
+
+let num1 = 56;
+let num2 = 12;
+let result = (num1/num2);
+console.log("The quotient of 56 and 12 is : "+ result);
+
+
+//The remainder of 281 divided by 9
+
+
+let a = 281;
+let b = 9;
+let requiredOutput= (a%b);
+console.log("The modulus of 281 and 9 is : "+ requiredOutput);
+
+
+
+//18 divided by 0
+
+const input1 = 18;
+const input2 = 0;
+ const ans = input1 / input2;
+ console.log(ans);
+
+ //The sum of string 56 and string 92
+
+ let myString = "56";
+ let oursString = "92";
+
+ let togetherString = (myString + oursString);
+
+ console.log(`My answer is ${togetherString}`);
+
+
+ //Using any comparison operator you wish on the numbers 42 and 52, make it evaluate to false.
+
+ let z = 42;
+ let z1 = 52;
+
+ if( z === z1){
+     console.log("True");
+ }
+ else{
+     console.log("False");
+ }
