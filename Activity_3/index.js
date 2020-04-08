243ea94f0b4d6fcef7d18643f47637a7a0e35837
devaryal96 <60721669+devaryal96@users.noreply.github.com>
2020-04-07 21:58:19 -0500

    Activity3Commit

---
+++ Activity_3/index.js
@@ -0,0 +1,15 @@
+
+ let studentName =['Roshni', 'Cheeku', 'Chanu'];
+
+
+
+
+for(let i=0; i<studentName.length; i++){
+    
+    const x = "rr";
+    console.log(x);
+    const a=   studentName.push(x);
+    console.log(a);
+    // alert(studentName);
+    // alert(studentName.length);
+}
